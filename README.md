9a875fc3ea54b3a9cf993f9480bca476d48e4fe7
Shine Kim <shinexkim@gmail.com>
2020-06-12 03:10:59 -0700

    Restored to New Repo

---
+++ README.md
@@ -0,0 +1,8 @@
+CS4550-Bento 
+A Recipe Search Web Application
+
+Bento is a web application that allows you to easily search for recipes without the popups/ads. With Bento, you must make an account to search for recipes. The sign-in process should be relatively simple. After joining, you can search for recipes, save recipes, update/edit your pantry ingredients, and more.
+
+We have chosen to use the Spoonacular API for this project. 
+
+Read the formal description here: https://drive.google.com/open?id=13dns6zOcXTN282gAG-y37B03c4ujpY36
---
+++ src/main/webapp/precache-manifest.abb0cc908e9f0cf40dcb383af759cc1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d957b5c9f4d319e07a8e0181661419d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "684f76b272966ef95063",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b1b8c3558c418440c126",
+    "url": "/static/css/main.59df92fd.chunk.css"
+  },
+  {
+    "revision": "684f76b272966ef95063",
+    "url": "/static/js/2.e43cfafb.chunk.js"
+  },
+  {
+    "revision": "607e3288612995f125be1f71ecc9c8c7",
+    "url": "/static/js/2.e43cfafb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1b8c3558c418440c126",
+    "url": "/static/js/main.6041eede.chunk.js"
+  },
+  {
+    "revision": "e9f11431df104b318877",
+    "url": "/static/js/runtime-main.61760d71.js"
+  }
+]);
